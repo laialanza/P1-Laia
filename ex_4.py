15895a727a2c827efa2e96b345628fe8d269fe82
laialanza <laiaipatch@hotmail.com>
2021-11-09 12:15:38 +0100

    First documents uploaded

---
+++ ex_4.py
@@ -0,0 +1,24 @@
+def run_length_encoding(seq):
+    compressed = []
+    count = 1
+    char = seq[0]
+    for i in range(1, len(seq)):
+        if seq[i] == char:
+            count = count + 1
+        else:
+            compressed.append([char, count])
+            char = seq[i]
+            count = 1
+    compressed.append([char, count])
+
+    compressed_seq = ''
+    for i in range(0, len(compressed)):
+      for j in compressed[i]:
+        compressed_seq += str(j)
+
+    return compressed_seq
+
+
+#seq = 'AACCCBBBBBAAAAFFFFFFFF'
+#list1 = run_length_encoding(seq)
+#print(list1)
