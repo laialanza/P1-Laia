4bf22af4ede65bc1c034f3f49130a27a895cb132
laialanza <laiaipatch@hotmail.com>
2021-11-10 15:56:34 +0100

    Ex5 improved

--- ex_5.py
+++ ex_5.py
@@ -10,22 +10,22 @@ def idct2(a):
     return idct(idct(a.T, norm='ortho').T, norm='ortho')
 
 #Need to install scikit-image in terminal: pip install scikit-image
-#from skimage.io import imread
-#from skimage.color import rgb2gray
-#import numpy as np
-#import matplotlib.pylab as plt
+from skimage.io import imread
+from skimage.color import rgb2gray
+import numpy as np
+import matplotlib.pylab as plt
 
 # read lena RGB image and convert to grayscale
-#im = rgb2gray(imread('C:/Users/laiai/OneDrive/Desktop/UNI/Encoding Systems Audio and Video/Lenna_(test_image).png'))
-#imF = dct2(im)
-#im1 = idct2(imF)
-
-# check if the reconstructed image is nearly equal to the original image
-#np.allclose(im, im1)
-# True
+directory = input("Introduce the directory of your image:  \n")
+im = rgb2gray(imread(directory))
+imF = dct2(im)
+im1 = idct2(imF)
 
 # plot original and reconstructed images with matplotlib.pylab
-#plt.gray()
-#plt.subplot(121), plt.imshow(im), plt.axis('off'), plt.title('original image', size=20)
-#plt.subplot(122), plt.imshow(im1), plt.axis('off'), plt.title('reconstructed image (DCT+IDCT)', size=20)
-#plt.show()
+plotting = input("Do you want to print the image and the reconstructed one? Y or N: \n")
+if (plotting.upper() == "Y"):
+    plt.gray()
+    plt.subplot(121), plt.imshow(im), plt.axis('off'), plt.title('original image', size=20)
+    plt.subplot(122), plt.imshow(im1), plt.axis('off'), plt.title('reconstructed image (DCT+IDCT)', size=20)
+    plt.show()
+
